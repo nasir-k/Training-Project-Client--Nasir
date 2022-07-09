d14c08f2bb43fbc4e36f8e2b36ec8dc32ffe7fdb
nkhan <nkhan@ischoolconnect.com>
2022-07-09 17:21:14 +0530

    added navbar and also added styling and make it responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SharedModule } from './shared/shared.module';
+import { NavbarComponent } from './components/header/components/navbar/navbar.component';
+import { SidebarComponent } from './components/header/components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, NavbarComponent, SidebarComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
